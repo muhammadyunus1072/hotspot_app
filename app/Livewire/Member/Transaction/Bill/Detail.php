b99c3a6250d594f73ebe11db4b2c0c903cf560c0
Muhammad Yunus <muhammadyunus1072@gmail.com>
2024-06-18 10:03:53 +0700

    fix

--- app/Livewire/Member/Transaction/Bill/Detail.php
+++ app/Livewire/Member/Transaction/Bill/Detail.php
@@ -145,7 +145,7 @@ class Detail extends Component
             }
             DB::commit();
 
-            if($transaction && $this->payment_method_id != PaymentMethod::MIDTRANS_ID)
+            if($transaction && $this->payment_method_id == PaymentMethod::MIDTRANS_ID)
             {
                 $this->redirectRoute('bill.checkout', $this->objId);
             }else{
