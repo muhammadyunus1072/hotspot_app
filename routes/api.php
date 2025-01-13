b4ec95e6be22fb34908d640789ecadb07a4d33e2
Muhammad Yunus <muhammadyunus1072@gmail.com>
2025-01-13 16:21:07 +0700

    test_C

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Member\BillController;
@@ -19,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/callback', [DashboardController::class, 'callback']);
 Route::post('/midtrans-callback', [BillController::class, 'callback']);
